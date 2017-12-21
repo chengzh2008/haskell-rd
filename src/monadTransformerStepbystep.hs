2b48b513bdb9f8e3cbbe4312e3b1523124582097
chengzh2008 <chengzh2008@gmail.com>
2017-12-21 15:42:34 -0800

    add more info in throwError

--- src/monadTransformerStepbystep.hs
+++ src/monadTransformerStepbystep.hs
@@ -1,5 +1,6 @@
 module Transformers where
 
+import Control.Monad.Except
 import Control.Monad.Identity
 import Data.Maybe (fromJust)
 import qualified Data.Map as Map
@@ -45,7 +46,11 @@ var = "x"
 -- function (plus 1): \x -> x + 1
 plusOne = Abs var (Var var `Plus` Lit 1)
 -- 12 + ((\x -> x) (4 + 2))
-exampleExp = Lit 12 `Plus` (App (Abs "x" (Var "x")) (Lit 4 `Plus` Lit 2))
+exampleExp = Lit 12 `Plus` (App (Abs var (Var var)) (Lit 4 `Plus` Lit 2))
+exampleWithTypeMisMatch1 = Lit 12 `Plus` plusOne
+exampleWithTypeMisMatch2 = Plus (Lit 1) (Abs var (Var var))
+exampleWithKeyNotFound = Var var
+exampleWithWrongApp = App (Lit 2) (Lit 1)
 
 -- ###############################
 -- convert eval0 to a monadic style
@@ -54,7 +59,8 @@ type Eval1 a = Identity a
 runEval1  :: Eval1 a -> a
 runEval1 = runIdentity
 
-eval1 :: Env -> Exp -> Eval1 Value
+
+eval1 ::Monad m => Env -> Exp -> m Value
 eval1 env (Lit i) = return $ IntVal i
 eval1 env (Var n) = return $ fromJust $ Map.lookup n env
 eval1 env (Plus e1 e2) = do
@@ -67,3 +73,46 @@ eval1 env (App e1 e2) = do
   val2 <- eval1 env e2
   case val1 of
     FunVal env' n body -> eval1 (Map.insert n val2 env') body
+
+-- ###############################
+-- add error handling to eval0
+
+type Eval2 a = ExceptT String Identity a
+runEval2 :: Eval2 a -> Either String a
+runEval2 = runIdentity . runExceptT
+
+eval2a :: Env -> Exp -> Eval2 Value
+eval2a env (Lit i) = return $ IntVal i
+eval2a env (Var n) = return $ fromJust $ Map.lookup n env
+eval2a env (Plus e1 e2) = do
+  IntVal i1 <- eval2a env e1
+  IntVal i2 <- eval2a env e2
+  return $ IntVal (i1 + i2)
+eval2a env (Abs n e) = return $ FunVal env n e
+eval2a env (App e1 e2) = do
+  val1 <- eval2a env e1
+  val2 <- eval2a env e2
+  case val1 of
+    FunVal env' n body -> eval2a (Map.insert n val2 env') body
+
+-- eval2a Map.empty typeError will have pattern match failure
+-- need to add error handling in eval2a
+
+eval2b :: Env -> Exp -> Eval2 Value
+eval2b env (Lit i) = return $ IntVal i
+eval2b env (Var n) = case Map.lookup n env of
+  Nothing -> throwError $ "Key not found: " ++ n
+  Just x -> return x
+eval2b env (Plus e1 e2) = do
+  e1' <- eval2b env e1
+  e2' <- eval2b env e2
+  case (e1', e2') of
+    (IntVal i1, IntVal i2) -> return $ IntVal (i1 + i2)
+    _ -> throwError $ "type Error: " ++ show e1 ++ " and " ++ show e2 ++ " can not be added together."
+eval2b env (Abs n e) = return $ FunVal env n e
+eval2b env (App e1 e2) = do
+  val1 <- eval2b env e1
+  val2 <- eval2b env e2
+  case val1 of
+    FunVal env' n body -> eval2b (Map.insert n val2 env') body
+    _ -> throwError $ "Application Error: " ++ show e1 ++ " is not a function."
